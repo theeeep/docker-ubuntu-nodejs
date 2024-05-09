93c5d82beb85ddca4e2fdccc1623152354896246
theeeep <ideepakrabindra@gmail.com>
2024-05-09 21:50:40 +0530

    feat: add response message to root route in main.js
    Add a response message to the root route in main.js to return a JSON object with a greeting.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  return;
+  return res.json({ message: "Hello from ubuntu-nodejs Container" });
 });
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
